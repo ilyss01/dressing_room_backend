673620f2b73399ae082d4255bce39b5ccaef66b6
Ilya Romanyuk <ilya.romanuck2014@yandex.ru>
2024-04-18 21:43:11 +0300

    Added some code

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import numpy as np  # performance stuff
+import cv2 as cv  # opencv
+import os  # debug levels
 import matplotlib.pyplot as plt
-import numpy as np
-import cv2
+import matplotlib
+
+
+def extractClothShape(cloth):
+    pass
+
+
+def resizeImage(sourceShape, destinationShape):
+    pass
+
+
+def replaceClothesRegion(sourceShape, destinationShape):
+    pass
 
 # Pseudocode:
 # cloth1 = image(file1)
@@ -15,9 +29,42 @@ import cv2
 #
 # return swappedImage
 
+# Sources:
+# https://machinelearningknowledge.ai/image-segmentation-in-python-opencv/
+# https://docs.opencv.org/4.x/d3/db4/tutorial_py_watershed.html
+# https://learnopencv.com/image-segmentation/
+# https://www.opencvhelp.org/tutorials/image-analysis/image-segmentation/
+# https://docs.opencv.org/3.4/d7/d4d/tutorial_py_thresholding.html
+
+
 def main():
-
-
+    # matplotlib backend. doesn't properly work on wayland
+    matplotlib.use('TkAgg')
+    debug = int(os.environ['DEBUG'])  # was taken from tinygrad
+    # sample_image = cv2.imread('human.jpg')
+    # img = cv2.cvtColor(sample_image, cv2.COLOR_BGR2RGB)
+    # plt.imshow(img)
+    # plt.show(sample_image)
+    img = cv.imread('human.jpg')
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    same_img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    ret, thresh = cv.threshold(gray, 0, 255,
+                               cv.THRESH_BINARY_INV+cv.THRESH_OTSU)
+    if debug == 1:
+        grid = plt.figure(figsize=(10, 7))
+        grid.add_subplot(2, 2, 1)
+        plt.imshow(img)
+        plt.axis('off')
+        plt.title('img')
+        grid.add_subplot(2, 2, 2)
+        plt.imshow(gray)
+        plt.axis('off')
+        plt.title('gray')
+        grid.add_subplot(2, 2, 3)
+        plt.imshow(same_img)
+        plt.axis('off')
+        plt.title('same_img')
+        plt.show()
 
 
 main()
