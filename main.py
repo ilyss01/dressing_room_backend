21848aa7596ae2a6c4b20bbf4ecca1f4f2fea3d3
Ilya Romanyuk <ilya.romanuck2014@yandex.ru>
2024-04-18 20:32:29 +0300

    init comit

---
+++ main.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import cv2
+
+# Pseudocode:
+# cloth1 = image(file1)
+# cloth2 = image(file2)
+#
+# clothShape1 = extractClothShape(cloth1)
+# clothShape2 = extractClothShape(cloth2)
+#
+# resizedClothRegion2 = resizeImage(clothShape2, clothShape1)
+#
+# swappedImage = replaceClothesRegion(cloth1, resizedClothRegion2)
+#
+# return swappedImage
+
+def main():
+
+
+
+
+main()
